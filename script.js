ff0ecab06651d11e95cdc62be1c7714d7284f4ae
kripapatel2006-prog <patelkripa2006@gmail.com>
2026-07-11 20:58:41 +0530

    Added trip planner, live weather feature and UI improvements

--- script.js
+++ script.js
@@ -38,3 +38,108 @@ if (searchBtn) {
 
     });
 }
+function calculateBudget(){
+
+    const destination=document.getElementById("destination").value;
+
+    const days=parseInt(document.getElementById("days").value);
+
+    if(!days || days<=0){
+
+        document.getElementById("result").innerHTML="Please enter valid days.";
+
+        return;
+
+    }
+
+    let costPerDay=0;
+
+    switch(destination){
+
+        case "Goa":
+            costPerDay=4000;
+            break;
+
+        case "Kerala":
+            costPerDay=3500;
+            break;
+
+        case "Ladakh":
+            costPerDay=6000;
+            break;
+
+        case "Jaipur":
+            costPerDay=3000;
+            break;
+
+    }
+
+    const total=days*costPerDay;
+
+    document.getElementById("result").innerHTML=
+    `Estimated Budget: ₹${total.toLocaleString("en-IN")}`;
+
+}
+async function getWeather() {
+    const destination = document.getElementById("destination").value;
+
+    let city = "";
+
+    switch (destination) {
+        case "Goa":
+            city = "Panaji";
+            break;
+
+        case "Kerala":
+            city = "Kochi";
+            break;
+
+        case "Ladakh":
+            city = "Leh";
+            break;
+
+        case "Jaipur":
+            city = "Jaipur";
+            break;
+    }
+
+    const apiKey = "5670e676f8741adfa97b4189caec3dff";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},IN&appid=${apiKey}&units=metric`;
+
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+
+        if (data.cod === 200) {
+
+            document.getElementById("weatherResult").innerHTML = `
+                <h3>🌤 Weather in ${data.name}</h3>
+                <p>🌡 Temperature: ${data.main.temp} °C</p>
+                <p>☁ Weather: ${data.weather[0].description}</p>
+                <p>💧 Humidity: ${data.main.humidity}%</p>
+                <p>💨 Wind Speed: ${data.wind.speed} m/s</p>
+            `;
+
+        } else {
+
+            document.getElementById("weatherResult").innerHTML =
+                "Weather not available.";
+
+        }
+
+    } catch (error) {
+
+        console.error(error);
+
+        document.getElementById("weatherResult").innerHTML =
+            "Unable to fetch weather.";
+
+    }
+
+}
+    
+
